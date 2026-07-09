84fc80de6ada38be4d933d7d12603987c47f8f3e
dianarifin <ngawidian@gmail.com>
2026-07-10 05:28:39 +0700

    adding custom commands

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,13 +1,23 @@
 import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
+import { Header } from "./components/header";
+import { InputBar } from "./components/input-bar";
 
 function App() {
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <textarea focused />
+    <box
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="#0D0D12"
+      width="100%"
+      height="100%"
+      gap={2}
+    >
+      <Header />
+        <box width="100%" >
+          <InputBar onSubmit={() => {}} />
+        </box>
       </box>
-    </box>
   );
 }
 
